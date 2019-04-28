4365ec01ebeb7bb736ef39c315c033f9cce63536
matacz <matacz@gmail.com>
2019-04-28 12:00:03 +0200

    Initial commit

---
+++ src/main/java/pl/sda/parsers/EquationParser.java
@@ -0,0 +1,36 @@
+package pl.sda.parsers;
+
+import pl.sda.model.Equation;
+
+public class EquationParser {
+    public Equation parse(String equationString) {
+
+        String[] split = equationString.split("=");
+
+        Double c = Double.parseDouble(split[1]);
+
+        String[] leftSide = split[0].split("x");
+        /*
+        Double a1;
+        if ("".equals(leftSide[0]) {
+            a1 = 1d;
+        } else{
+            a1 = Double.parseDouble(leftSide[0]);
+
+            poniżej to samo, ale z użyciem tzw. operatora trójargumentowego:*/
+        Double a = "".equals(leftSide[0]) ? 1d : parseNonEmptyA(leftSide[0]);
+
+        Double b = leftSide.length == 1 ? 0 : Double.parseDouble(leftSide[1]);
+
+        Equation equation = new Equation();
+        equation.setA(a);
+        equation.setB(b);
+        equation.setC(c);
+
+        return equation;
+    }
+
+    private Double parseNonEmptyA(String nonEmptyA) {
+        return "-".equals(nonEmptyA) ? -1d : Double.parseDouble(nonEmptyA);
+    }
+}
